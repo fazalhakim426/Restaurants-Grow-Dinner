6df1a13ee41c089bc5b703354264e9f0910bbfa4
Fazal Hakim <hakimfazal426@gmail.com>
2022-04-19 12:43:16 +0500

    categories

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -15,7 +15,7 @@ class CategoryResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id' => $this->name,
+            'id' => $this->id,
             'name' => $this->name,
              'icon' => env('APP_URL').'/'. $this->icon,
              'active' => $this->active,
